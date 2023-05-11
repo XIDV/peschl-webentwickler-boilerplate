576db9daa5ae30e68267c3cbe657c6c7823392e8
Sebastian Pesch <sebastian.peschl@xidv.de>
2023-05-11 16:02:05 +0200

    Dynamisches und Benutzerdefiniertes ein-, ausblenden d. Menüs jezt OK

--- js/main.js
+++ js/main.js
@@ -1,35 +1,32 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', dcle => {
+$(() => {
     console.log('Ready for action ....');
     
+    const mainMenu = $('#pageNav');
+    const hButton = $('#hButton');
     
-    const mainMenu = document.querySelector('.pageNav');
-    const hButton = document.querySelector('#hButton');
-
+    let vpIsDesktop = false;
+    let mainMenuVisible = false;
     
-    setMenuVisibility();
+    setIsDesktop();
 
-    window.addEventListener('resize', setMenuVisibility);
-    document.querySelector('#hButton').addEventListener('click', toggleMenuVisibility);
+    $(window).on('resize', setIsDesktop);
+    hButton.on('click', toggleMenu);
 
-    function setMenuVisibility() {
-        window.innerWidth >= 820 ? mainMenu.dataset.show = 'true' : mainMenu.dataset.show = 'false';
-        window.innerWidth >= 820 ? hButton.style.display = 'none' : hButton.style.display = 'block';
-        setClass();
-    }
-    
-    function toggleMenuVisibility() {
-        mainMenu.dataset.show === 'true' ? mainMenu.dataset.show = 'false' : mainMenu.dataset.show = 'true';
-        setClass();
+
+    function setIsDesktop() {
+        $(window).innerWidth() >= 820 ? vpIsDesktop = true : vpIsDesktop = false;
+        toggleMenu();
     }
 
-    function setClass() {
-        if(mainMenu.dataset.show === 'true') {
-            mainMenu.classList.remove('hide');
-        } else if(mainMenu.dataset.show === 'false') {
-            mainMenu.classList.add('hide');
+    function toggleMenu(e) {
+        if(e && !vpIsDesktop) {
+            mainMenu.slideToggle('fast', 'linear');
+            mainMenuVisible = !mainMenuVisible;
+        } else {
+            vpIsDesktop ? mainMenu.slideDown('fast', 'linear') : mainMenu.slideUp('fast', 'linear');
+            mainMenuVisible = !mainMenuVisible;
         }
     }
-
 });
